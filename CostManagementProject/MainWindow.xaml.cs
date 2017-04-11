29506eeae4d4046b54cbd026c547f7b147713788
OstapGereley <ostap.gereley@gmail.com>
2017-04-11 10:36:56 +0300

    Initial Commint

---
+++ CostManagementProject/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using CostManagementProject.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace CostManagementProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<YearGrowth> YearStats = new ObservableCollection<YearGrowth>();
+        private int yearsCount;
+        public int YearsCount
+        {
+            get
+            {
+                return yearsCount;
+            }
+            set
+            {
+                yearsCount = value;
+                ElementsCountLabel.Content = value;
+            }
+        }
+        public MainWindow()
+        {            
+            InitializeComponent();
+            YearStats.Add(new YearGrowth(1, 1, 1, 1, 1, 1, 1, 1));
+            YearsCount = YearStats.Count;
+            YearGrowthGrid.ItemsSource = YearStats;
+
+
+        }
+
+        private void AddButtonClick(object sender, RoutedEventArgs e)
+        {
+            YearsCount++;
+            YearStats.Add(new YearGrowth());
+        }
+
+        private void SubButtonClick(object sender, RoutedEventArgs e)
+        {
+            YearsCount--;
+            YearStats.RemoveAt(YearStats.Count - 1);
+        }
+    }
+}
---
+++ CostManagementProject/Models/YearGrowth.cs
@@ -0,0 +1,28 @@
+﻿namespace CostManagementProject.Models
+{
+    public class YearGrowth
+    {
+        public double Year { get; set; }
+        public double NetProfit { get; set; }
+        public double SalesNetIncome { get; set; }
+        public double Cost { get; set; }
+        public double AverageAssets { get; set; }
+        public double AverageFixedAssets { get; set; }
+        public double AverageCurrentAssets { get; set; }
+        public double EmployeeCount { get; set; }
+
+        public YearGrowth() { }
+
+        public YearGrowth(double year, double netProfit, double salesNetIncome, double cost, double averageAssets, double averageFixedAssets, double averageCurrentAssets, double employeeCount)
+        {
+            Year = year;
+            NetProfit = netProfit;
+            SalesNetIncome = salesNetIncome;
+            Cost = cost;
+            AverageAssets = averageAssets;
+            AverageFixedAssets = averageFixedAssets;
+            AverageCurrentAssets = averageCurrentAssets;
+            EmployeeCount = employeeCount;
+        }
+    }
+}
